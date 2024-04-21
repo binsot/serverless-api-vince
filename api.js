69ea3983605003fad779a2dda78a98be1cd3c44a
almonte <janricoalmonte0712@gmail.com>
2024-04-21 22:14:56 +0800

    Initial commit

---
+++ api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const router = require('./routes/author'); const mongoose = require('mongoose'); const cors = require('cors');
+
+const app = express();
+// your mongoDB Cloud URL
+const dbCloudUrl =
+    'mongodb+srv://vincepelaez05:<password>@binsotil.hhdqokn.mongodb.net/?retryWrites=true&w=majority&appName=binsotil';
+// your mongoDB Cloud URL
+const dbLocalUrl = 'mongodb://localhost:27017/serverless-api-pelaez';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+    .connect(dbCloudUrl || dbLocalUrl)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Failed to connect to MongoDB', error));
+    
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless (app);
